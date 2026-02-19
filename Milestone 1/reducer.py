762434d81628f8c9d53fe96aca956f3bacbf38c7
lateenalhurasen <lateenalhurasen@gmail.com>
2026-02-19 20:32:44 +0300

    Update reducer.py

--- Milestone 1/reducer.py
+++ Milestone 1/reducer.py
@@ -1,29 +1,27 @@
-#Reducer used for all tasks
+#reducer used for all mapper tasks
 import sys
 
 current_key = None
-current_sum = 0
+current_total = 0
 
 for line in sys.stdin:
-    line = line.rstrip("\n")
-    if not line:
+    line = line.strip()  #Remove whitespace
+    if not line:  #If the line is empty skip
         continue
 
     try:
-        key, value = line.split("\t", 1)
-        value = int(value)
-    except ValueError:
-        continue  # skip malformed lines
+        key, count = line.split('\t', 1)
+        count = int(count)
+    except ValueError: #If the line isn't in the correct format ignore it
+        continue
 
-    if current_key is None:
+    if current_key == key:
+        current_total += count
+    else:
+        if current_key is not None:
+            print(f"{current_key}\t{current_total}")
         current_key = key
-
-    if key != current_key:
-        print(f"{current_key}\t{current_sum}")
-        current_key = key
-        current_sum = 0
-
-    current_sum += value
+        current_total = count
 
 if current_key is not None:
-    print(f"{current_key}\t{current_sum}")
+    print(f"{current_key}\t{current_total}")
